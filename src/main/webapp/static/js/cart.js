2f0038ba8b38bf660336feaeffc7164cafa0457a
Tamas Kovacs <kovacstamas990@gmail.com>
2021-08-04 11:47:06 -0700

    Cart content tabble is formatted

--- src/main/webapp/static/js/cart.js
+++ src/main/webapp/static/js/cart.js
@@ -19,9 +19,13 @@ function init() {
         let nameOfProduct = document.createElement("th");
         nameOfProduct.scope = "col";
         nameOfProduct.innerText = "Name of product";
+        let downButtonHeader = document.createElement("th");
+        downButtonHeader.scope = "col"
         let quantityHeader = document.createElement("th");
         quantityHeader.innerText = "quantity";
         quantityHeader.scope = "col"
+        let upButtonHeader = document.createElement("th");
+        upButtonHeader.scope = "col"
         let unitPriceHeader = document.createElement("th");
         unitPriceHeader.innerText = "Unit price";
         unitPriceHeader.scope = "col"
@@ -29,7 +33,9 @@ function init() {
         deleteButtonHeader.scope = "col"
         deleteButtonHeader.innerText = "Delete";
         header.appendChild(nameOfProduct);
+        header.appendChild(downButtonHeader)
         header.appendChild(quantityHeader);
+        header.appendChild(upButtonHeader);
         header.appendChild(unitPriceHeader);
         header.appendChild(deleteButtonHeader);
         table.appendChild(header);
@@ -42,33 +48,33 @@ function init() {
             let price = document.createElement("td");
             price.innerText = product[1].defaultPrice;
             let quantity = document.createElement("td");
+            let downButton = document.createElement("td");
+            let db = document.createElement("button");
+            db.type = "button";
+            db.innerText = "-";
+            db.className = "btn btn-primary"
+            downButton.appendChild(db);
+            quantity.appendChild(downButton)
+            let upButton = document.createElement("td");
+            let ub = document.createElement("button");
+            ub.type = "button";
+            ub.innerText = "+";
+            ub.className = "btn btn-primary"
+            upButton.appendChild(ub)
             quantity.innerText = product[1].quantity;
             let del = document.createElement("td");
-            del.innerText = "delete";
+            let delB = document.createElement("button");
+            delB.type = "button";
+            delB.innerText = "Delete";
+            delB.className = "btn btn-primary"
+            del.appendChild(delB)
             row.appendChild(name);
+            row.appendChild(downButton);
             row.appendChild(quantity);
+            row.appendChild(upButton);
             row.appendChild(price);
             row.appendChild(del);
             table.appendChild(row);
         }
-
-        // <table style="width:100%">
-        //     <tr>
-        //         <th>Firstname</th>
-        //         <th>Lastname</th>
-        //         <th>Age</th>
-        //     </tr>
-        //     <tr>
-        //         <td>Jill</td>
-        //         <td>Smith</td>
-        //         <td>50</td>
-        //     </tr>
-        //     <tr>
-        //         <td>Eve</td>
-        //         <td>Jackson</td>
-        //         <td>94</td>
-        //     </tr>
-        // </table>
-
     }
 }
