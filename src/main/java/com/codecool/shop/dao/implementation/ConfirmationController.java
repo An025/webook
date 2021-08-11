e2ba4b436284ef059b7d401b0b4e1d1d3c6b9770
Ani <jancso.anita@gmail.com>
2021-08-11 18:04:24 +0200

    display orders in confirmation page

--- src/main/java/com/codecool/shop/dao/implementation/ConfirmationController.java
+++ src/main/java/com/codecool/shop/dao/implementation/ConfirmationController.java
@@ -2,6 +2,7 @@ package com.codecool.shop.dao.implementation;
 
 import com.codecool.shop.config.TemplateEngineUtil;
 import com.codecool.shop.dao.implementation.*;
+import com.codecool.shop.model.Product;
 import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.context.WebContext;
 
@@ -11,6 +12,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.ArrayList;
 
 @WebServlet(urlPatterns = {"/confirmation"})
 public class ConfirmationController extends HttpServlet {
@@ -20,8 +22,9 @@ public class ConfirmationController extends HttpServlet {
         TemplateEngine engine = TemplateEngineUtil.getTemplateEngine(req.getServletContext());
         WebContext context = new WebContext(req, resp, req.getServletContext());
         BillingInfoMem billingInfo = CartDaoMem.getInstance().getBillingInfoMem();
+        ArrayList<Product> productsInfo = CartDaoMem.getInstance().getAll();
         context.setVariable("billingInfo", billingInfo);
-
+        context.setVariable("productsList", productsInfo);
         engine.process("product/confirmation.html", context, resp.getWriter());
     }
 }
