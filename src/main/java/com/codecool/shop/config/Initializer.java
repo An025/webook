13d5cc7c1441c528d58534095df0b53eea422396
Ani <jancso.anita@gmail.com>
2021-08-12 18:34:03 +0200

    add programming jpg and programming details

--- src/main/java/com/codecool/shop/config/Initializer.java
+++ src/main/java/com/codecool/shop/config/Initializer.java
@@ -30,17 +30,17 @@ public class Initializer implements ServletContextListener {
         supplierDataStore.add(lenovo);
 
         //setting up a new product category
-        ProductCategory gastronomy = new ProductCategory("Gastronomy", "Hardware", "A tablet computer, commonly shortened to tablet, is a thin, flat mobile computer with a touchscreen display.");
-        productCategoryDataStore.add(gastronomy);
+        ProductCategory programming = new ProductCategory("Programming", "Hardware", "A tablet computer, commonly shortened to tablet, is a thin, flat mobile computer with a touchscreen display.");
+        productCategoryDataStore.add(programming);
         ProductCategory romantic  = new ProductCategory("Romantic", "Hardware", "A tablet computer, commonly shortened to tablet, is a thin, flat mobile computer with a touchscreen display.");
         productCategoryDataStore.add(romantic);
         ProductCategory family = new ProductCategory("Family", "Hardware", "A tablet computer, commonly shortened to tablet, is a thin, flat mobile computer with a touchscreen display.");
         productCategoryDataStore.add(family);
 
         //setting up products and printing it
-        productDataStore.add(new Product("Gastronomy1", 49, "USD", "Fantastic price. Large content ecosystem. Good parental controls. Helpful technical support.", gastronomy, amazon));
-        productDataStore.add(new Product("Gastronomy2", 479, "USD", "Keyboard cover is included. Fanless Core m5 processor. Full-size USB ports. Adjustable kickstand.", gastronomy, lenovo));
-        productDataStore.add(new Product("Gastronomy3", 89, "USD", "Amazon's latest Fire HD 8 tablet is a great value for media consumption.", gastronomy, amazon));
+        productDataStore.add(new Product("Cracking the Coding Interview", 35, "USD", "I' am not a recruiter. I am a software engineer. And as such, I know what it's like to be asked to whip up brilliant algorithms on the spot and then write flawless code on a whiteboard. I've been through this as a candidate and as an interviewer.", programming, amazon));
+        productDataStore.add(new Product("Clean Code", 32, "USD", "Even bad code can function. But if code isn't clean, it can bring a development organization to its knees. Every year, countless hours and significant resources are lost because of poorly written code. But it doesn't have to be that way.", programming, lenovo));
+        productDataStore.add(new Product("Interviews in Java", 47, "USD", "The core of EPI is a collection of over 250 problems with detailed solutions. The problems are representative of interview questions asked at leading software companies. The problems are illustrated with 200 figures, 300 tested programs, and 150 additional variants.", programming, amazon));
         productDataStore.add(new Product("Family1", 89, "USD", "Amazon's latest Fire HD 8 tablet is a great value for media consumption.", family, amazon));
         productDataStore.add(new Product("Family2", 49, "USD", "Fantastic price. Large content ecosystem. Good parental controls. Helpful technical support.", family, lenovo));
         productDataStore.add(new Product("Family3", 479, "USD", "Keyboard cover is included. Fanless Core m5 processor. Full-size USB ports. Adjustable kickstand.", family, lenovo));
