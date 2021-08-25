34c16f143c39b432e5646ba4c5332a1ddb516094
Ani <jancso.anita@gmail.com>
2021-08-25 20:58:06 +0200

    implement database connection with connection.properties

--- src/main/java/com/codecool/shop/config/Init.java
+++ src/main/java/com/codecool/shop/config/Init.java
@@ -1,15 +1,31 @@
 package com.codecool.shop.config;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.Properties;
 
 public  class Init {
-    public static String getInitType(){
-        try (FileReader reader = new FileReader("src/main/resources/connection.properties");){
+    private static Logger logger = LoggerFactory.getLogger(Initializer.class);
+    protected static String USER_NAME;
+    protected static String PASSWORD;
+    protected static String DATABASE;
+
+    public static String getInitType() {
+        try (FileReader reader = new FileReader("src/main/resources/connection.properties");) {
             Properties prop = new Properties();
             prop.load(reader);
-            return prop.getProperty("data");
+            if (prop.getProperty("data").equals("memory")) {
+                return "memory";
+            } else {
+                DATABASE = prop.getProperty("DATABASE");
+                USER_NAME = prop.getProperty("USER_NAME");
+                PASSWORD = prop.getProperty("PASSWORD");
+                return "database";
+            }
+
         } catch (IOException e) {
             e.getMessage();
         }
