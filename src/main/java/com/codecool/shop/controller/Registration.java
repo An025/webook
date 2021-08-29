2e4cb28abb4454e93a52ead4345fb3a0adbe2278
Ani <jancso.anita@gmail.com>
2021-08-29 06:32:58 +0200

    Create registration page

--- src/main/java/com/codecool/shop/controller/Registration.java
+++ src/main/java/com/codecool/shop/controller/Registration.java
@@ -1,2 +1,28 @@
-package com.codecool.shop.controller;public class Registration {
+package com.codecool.shop.controller;
+
+import com.codecool.shop.config.TemplateEngineUtil;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.WebContext;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+
+@WebServlet(urlPatterns = {"/registration"})
+public class Registration extends HttpServlet {
+    private static Logger logger = LoggerFactory.getLogger(BillingInfo.class);
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        TemplateEngine engine = TemplateEngineUtil.getTemplateEngine(req.getServletContext());
+        WebContext context = new WebContext(req, resp, req.getServletContext());
+        engine.process("product/registration.html", context, resp.getWriter());
+        logger.info("Load Registration page");
+    }
 }
