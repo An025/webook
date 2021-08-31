0cf0607e95edb790bf7d45e0a2fe4b35500aa168
Ani <jancso.anita@gmail.com>
2021-08-31 06:00:40 +0200

    Implement and display login page

---
+++ src/main/java/com/codecool/shop/controller/Login.java
@@ -0,0 +1,29 @@
+package com.codecool.shop.controller;
+
+import com.codecool.shop.config.TemplateEngineUtil;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.WebContext;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+
+@WebServlet(urlPatterns = {"/login"})
+public class Login extends HttpServlet {
+    private static Logger logger = LoggerFactory.getLogger(Login.class);
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        TemplateEngine engine = TemplateEngineUtil.getTemplateEngine(req.getServletContext());
+        WebContext context = new WebContext(req, resp, req.getServletContext());
+        engine.process("product/login.html", context, resp.getWriter());
+        logger.info("Load login page");
+    }
+}
